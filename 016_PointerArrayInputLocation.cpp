8dea1f016b75b21fea6dd74fd6eb72b081670b6c
Aan Nea <90422412+aannea@users.noreply.github.com>
2023-06-05 21:30:34 +0700

    Create 016_PointerArrayInputLocation.cpp

---
+++ 016_PointerArrayInputLocation.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[10];
+    int b;
+    int *c = a;
+
+    cout << "Masukkan 10 Array" << endl;
+    b=0;
+    while (b<10){
+        cout << "Array Ke " << b << " : ";
+        cin >> a[b];
+        b++;
+    }
+    system("pause");
+    system("cls");
+    cout << "Pemanggilan menggunakan Pointer" << endl;
+    b=0;
+    while (b<10){
+        cout << "Array Ke " << b << " : " << c[b] << endl;
+        b++;
+    }
+    system("pause");
+    system("cls");
+    cout << "Menampilkan alamat memory" << endl;
+    b=0;
+    while (b<10){
+        cout << "Array Ke " << b << " : " << &c[b] << endl;
+        b++;
+    }
+    return 0;
+}
